03abb284f21f587358c52ab600bd388b8c943746
milena <mivabow@gmail.com>
2023-06-13 23:54:43 +0800

    Added carousel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import { HotelsContextProvider } from "../contexts/HotelsContext";
 import { SearchContextProvider } from "../contexts/SearchContext";
 import { AvailableHotels } from "../sections/AvailableHotels";
 import { HomesGuestsLoves } from "../sections/HomesGuestsLoves";
 import { TopSection } from "../sections/TopSection";
+import { Loader } from "./Loader";
 
 export const App = () => {
   return (
@@ -12,7 +13,9 @@ export const App = () => {
       <SearchContextProvider>
         <HotelsContextProvider>
           <TopSection />
-          <AvailableHotels />
+          <Suspense fallback={<Loader />}>
+            <AvailableHotels />
+          </Suspense>
         </HotelsContextProvider>
       </SearchContextProvider>
       <HomesGuestsLoves />
--- src/components/HotelsList/HotelsList.jsx
+++ src/components/HotelsList/HotelsList.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-import classNames from "classnames";
-import { array, string } from "prop-types";
+import { array } from "prop-types";
 
+import {
+  CustomLeftArrow,
+  CustomRightArrow,
+} from "../CustomButton/CustomButton";
 import { HotelCard } from "../HotelCard";
 import "./HotelsList.css";
 
-export const HotelsList = ({ data, className }) => {
+export const HotelsList = ({ data }) => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 1920, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 2,
+    },
+  };
+
   return (
-    <ul className={classNames("list col-lg-12 col-sm-6", className)}>
+    <Carousel
+      responsive={responsive}
+      className="list col-lg-12 col-sm-6"
+      customLeftArrow={<CustomLeftArrow />}
+      customRightArrow={<CustomRightArrow />}
+    >
       {data.map((el) => (
         <HotelCard el={el} key={el.id} />
       ))}
-      <div className="circle">
-        <div className="arrow"></div>
-      </div>
-    </ul>
+    </Carousel>
   );
 };
 
 HotelsList.propTypes = {
   data: array.isRequired,
-  className: string,
 };
